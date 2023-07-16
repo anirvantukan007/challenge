139e297528e2055e7a5c31a3a4982c7af3004159
anirvantukan007 <anirvantukan007@gmail.com>
2023-07-17 02:28:52 +0530

    Updated-variables

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #This will deploy resource group in Azure portal
 resource "azurerm_resource_group" "main" {
   name     = "rg-az-test-pub"
-  location = var.location
+  location = "West US"
 
   tags = {
     environment = "test"
