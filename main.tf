5ff0fadcb2582737d5daab7c1bd940c3dbdd2ad3
anirvantukan007 <anirvantukan007@gmail.com>
2023-07-17 02:51:09 +0530

    Updated-script

--- main.tf
+++ main.tf
@@ -8,6 +8,20 @@ resource "azurerm_resource_group" "main" {
   }
 }
 
-module "storage" {
-  source = "./module/storage/"
+resource "azurerm_storage_account" "main" {
+  name                     = "stazpubdev"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
 }
