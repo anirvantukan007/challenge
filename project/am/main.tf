be4790a69bf06f28df26da33e90a847b1f72c35e
anirvantukan007 <anirvantukan007@gmail.com>
2023-07-17 13:58:52 +0530

    Updated-script

--- project/am/main.tf
+++ project/am/main.tf
@@ -1,7 +1,23 @@
-data "azurerm_client_config" "main" {}
-
+#This will deploy resource group in Azure portal
 resource "azurerm_resource_group" "main" {
-  name      = "rg-az-vm-pub"
-  location  = "West US"
-  tags      = var.tags
+  name     = "rg-az-test-pub"
+  location = "West US"
+
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "stazpubdev"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
 }
