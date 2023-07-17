6c12059f4aa38fea5b629b9d7f59472f9437e9ff
anirvantukan007 <anirvantukan007@gmail.com>
2023-07-17 17:09:46 +0530

    Updated-script

--- project/js/main.tf
+++ project/js/main.tf
@@ -4,24 +4,15 @@ data "external" "vm_json" {
   program = ["sh", "-c", "terraform show -json | jq '.values.root_module.resources[] | select(.type == \"azurerm_virtual_machine\" and .name == \"vmwindev\")'"]
 }
 
-data "azurerm_resource_group" "main" {
-  name = "rg-az-test-pub"
-}
-
-data "azurerm_storage_account" "main" {
-  name                = "stazpubdev"
-  resource_group_name = data.azurerm_resource_group.main
-}
-
 resource "azurerm_storage_container" "main" {
   name                  = "json"
-  storage_account_name  = data.azurerm_storage_account.main.name
+  storage_account_name  = "stazpubdev"
   container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "main" {
   name                   = "azure_json"
-  storage_account_name   = data.azurerm_storage_account.main.name
+  storage_account_name   = "stazpubdev"
   storage_container_name = azurerm_storage_container.main.name
   type                   = "Block"
   source_content         = data.external.vm_json.result
