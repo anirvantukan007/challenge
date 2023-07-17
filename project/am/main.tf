22c5e95555943bcbdbe3f4ae9ea63305f0b94e6d
anirvantukan007 <anirvantukan007@gmail.com>
2023-07-17 14:24:02 +0530

    Updated-tfstate

--- project/am/main.tf
+++ project/am/main.tf
@@ -5,17 +5,3 @@ resource "azurerm_resource_group" "main" {
 
   tags = var.tags
 }
-
-resource "azurerm_storage_account" "main" {
-  name                     = "stazpubdev"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "main" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.main.name
-  container_access_type = "private"
-}
