b3efdbe6985ad91d80192daf4c54edc7a7a3c31b
anirvantukan007 <anirvantukan007@gmail.com>
2023-07-17 11:22:18 +0530

    Updated-resource group

--- project/am/main.tf
+++ project/am/main.tf
@@ -1,21 +1,7 @@
 data "azurerm_client_config" "main" {}
 
 resource "azurerm_resource_group" "main" {
-  name      = "rg-az-test-pub"
+  name      = "rg-az-vm-pub"
   location  = "West US"
   tags      = var.tags
-}
-
-resource "azurerm_storage_account" "main" {
-  name                     = "stazpubdev"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "main" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.main.name
-  container_access_type = "private"
 }
