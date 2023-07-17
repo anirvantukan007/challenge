2c5a56c429652d6b3c3f3c040c0b72787c5af5bf
anirvantukan007 <anirvantukan007@gmail.com>
2023-07-17 14:19:18 +0530

    Updated-script

--- project/am/main.tf
+++ project/am/main.tf
@@ -3,9 +3,7 @@ resource "azurerm_resource_group" "main" {
   name     = "rg-az-test-pub"
   location = "West US"
 
-  tags = {
-    environment = "test"
-  }
+  tags = var.tags
 }
 
 resource "azurerm_storage_account" "main" {
